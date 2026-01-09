564e7538c468483897d439be5ef6089dd82d1eff
fauzanodji-beep <fauzanodji@gmail.com>
2026-01-09 14:34:32 +0700

    Menambahkan Validasi Nilai

--- NilaiSiswaApp/Program.cs
+++ NilaiSiswaApp/Program.cs
@@ -8,4 +8,13 @@ Console.Write("Nama Siswa: ");
 nama = Console.ReadLine();
 
 Console.Write("Nilai: ");
-nilai = int.Parse(Console.ReadLine();
+nilai = int.Parse(Console.ReadLine());
+
+if (nilai >= 75 )
+{
+    Console.WriteLine("Selamat, Lulus");
+}
+else
+{
+    Console.WriteLine("LU PINTER BANGET YAAA");
+}
